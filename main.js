719e9314111d84873d80e658871ba8793cc9abd8
joshguion <joshguion@icloud.com>
2015-10-22 09:02:09 -0400

    updated tabs

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ $('#profilePic').append("<img src='" + user.avatar_url + "'>");
 $('.name').text(user.name);
 $('.login').text(user.login);
 $('.location').text(user.location);
-// $('.joinedOn').text("Joined on " + moment(users.created_at).format("ll"));
 $('.followers').text(user.followers);
 $('.starred').text(user.starred);
 $('.following').text(user.following);
@@ -22,23 +21,23 @@ var coreRepo = _.each(repo, function(item){
 $('.repo').append(filteredRepo);
 
 //Tabbies
-$('.contTab').click(function(event){
+$('#contTab').click(function(event){
   event.preventDefault();
-  $('#contInfo').css('display', 'block')
-  $('#repoInfo').css('display', 'none')
-  $('#pubInfo').css('display', 'none')
+  $('.contInfo').css('display', 'block')
+  $('.repo').css('display', 'none')
+  $('.pubInfo').css('display', 'none')
 });
 
-$('.repoTab').click(function(event){
+$('#repoTab').click(function(event){
   event.preventDefault();
-  $('#contInfo').css('display', 'none')
-  $('#repoInfo').css('display', 'block')
-  $('#pubInfo').css('display', 'none')
+  $('.contInfo').css('display', 'none')
+  $('.repo').css('display', 'block')
+  $('.pubInfo').css('display', 'none')
 });
 
-$('.pubTab').click(function(event){
+$('#pubTab').click(function(event){
   event.preventDefault();
-  $('#contInfo').css('display', 'none')
-  $('#repoInfo').css('display', 'none')
-  $('#pubInfo').css('display', 'block')
+  $('.contInfo').css('display', 'none')
+  $('.repo').css('display', 'none')
+  $('.pubInfo').css('display', 'block')
 });
